ac069e13e4aa2829ba58f414c29f17240bb98f28
princek <princesamuelpks@gmail>
2024-02-16 12:26:39 +0000

    Update loading.dart

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:signee/screens/onboarding.dart';
 
@@ -33,24 +34,41 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(
-          child: Text(
-            's i g n e e',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w900,
-              color: Colors.purple[700],
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/icons/playstore.png',
+            height: 200,
+            width: 200,
+          ),
+          SizedBox(
+            child: Center(
+              child: DefaultTextStyle(
+                style: const TextStyle(
+                  fontSize: 30.0,
+                  fontFamily: 'Bobbers',
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                child: AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      's i g n e e',
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
-        ),
-        const SizedBox(height: 20),
-        CircularProgressIndicator(
-          color: Colors.purple[700],
-        ),
-      ],
+          const SizedBox(height: 20),
+          CircularProgressIndicator(
+            color: Colors.purple[700],
+          ),
+        ],
+      ),
     ));
   }
 }
